04ac3be84255f5458cc2dcdd532ee83a5d292f2e
silvarge <sep.non2@gmail.com>
2023-07-31 14:10:01 +0900

    make token practice

--- class/section01/01-01-token/qqq.js
+++ class/section01/01-01-token/qqq.js
@@ -1,1 +1,9 @@
 console.log("안녕하세요!!")
+
+function getToken(){
+    const result = String(Math.floor(Math.random() * 1000000)).padStart(6, 0);
+    // 6글자를 기준으로 사라진 자리를 padStart의 두 번째 값으로 채워줌
+    console.log(result)
+}
+
+getToken()
